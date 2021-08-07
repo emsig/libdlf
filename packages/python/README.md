b2d0910e38d555879c7afb620264981c3efa07e7
prisae <mail@werthmuller.org>
2021-08-07 16:57:58 +0200

    Remove packages for this branch

--- packages/python/README.md
+++
@@ -1,3 +0,0 @@
-# Python package of libdlf
-
-Currently this is just a stub. To be done.
--- packages/python/libdlf/__init__.py
+++
@@ -1,2 +0,0 @@
-print("Stub package for `libdlf`.")
-print("Please go to https://github.com/emsig/libdlf to help shaping it.")
